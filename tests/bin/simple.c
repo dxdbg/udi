af6d2c9daa48b9176fc8609e310e6a700e01b858
Dan McNulty <mcnulty@users.noreply.github.com>
2012-01-31 22:16:59 -0600

    Add debug printing to test binaries.

--- tests/bin/simple.c
+++ tests/bin/simple.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "bin_lib.h"
+
 int function1() {
-    printf("In function1\n");
+    bin_printf("In function1\n");
     return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
-    printf("Before function1\n");
+    init_bin();
+
+    bin_printf("Before function1\n");
     function1();
-    printf("After function1\n");
+    bin_printf("After function1\n");
 
     // This is a temporary workaround until
     // can detect implicit exits via libc
